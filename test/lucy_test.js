43dcd239662915d6c6c01273097076e520a11b11
Rafael Almeida <rafael@iswe.co.nz>
2014-01-29 20:27:51 +1300

    added unit tests

--- test/lucy_test.js
+++ test/lucy_test.js
@@ -28,14 +28,62 @@ exports.lucy = {
     done();
 
   },
-  'no args': function (test) {
+  'mapCollection': function (test) {
     test.expect(1);
     // tests here
     test.equal(lucy.mapCollection('test'), lucy, 'mapCollection should return lucy for chaining.');
-    
-    
-
 
     test.done();
+  },
+
+  'asPromise - success': function (test) {
+
+    var objx = {
+      testfn: function (arg1, arg2, callback) {
+        console.log('testfn called! arguments: ' + JSON.stringify(arguments));
+        callback(null, (arg1 + arg2));
+      }
+    };
+
+    test.expect(1);
+
+    lucy.asPromise(objx, 'testfn');
+
+    objx.testfn(1, 1).then(function (total) {
+
+      test.equal(total, 2, 'promise result is what is returned by the async callback.');
+
+      test.done();
+
+    });
+  },
+
+  'asPromise - error': function (test) {
+
+    var errorObj = new Error('async exception');
+
+    var objx = {
+      testfnError: function (arg1, arg2, callback) {
+        callback(errorObj);
+      }
+    };
+
+    test.expect(1);
+
+    lucy.asPromise(objx, 'testfnError');
+
+    objx.testfnError(1, 1).then(function () {
+
+      test.ok(false, 'should not be called!');
+      test.done();
+
+    },
+    function (error) {
+
+      test.equal(error, errorObj, 'correct error obj');
+
+      test.done();
+    });
   }
+
 };
